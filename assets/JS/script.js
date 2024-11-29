0dbb2bcc83d7d21084614f2fe6c27025a456223b
Mav <chinedundulue156@gmail.com>
2024-11-29 07:03:33 +0100

    Updated contact page

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,15 +5,3 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
-
-
-//Navbar special feature
-const nav = document.getElementById('nav');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 100) {
-        nav.classList.add('nav_black');
-    } else {
-        nav.classList.remove('nav_black');
-    }
-});
